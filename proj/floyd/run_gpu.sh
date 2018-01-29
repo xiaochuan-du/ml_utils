5d8ec10b8371832c6bf4433c54216d51bd2a77fe
xiaochuan-du <xiaochuan.du@healscitech.com>
2018-01-29 23:09:54 +0800

    fix submit issue

--- proj/floyd/run_gpu.sh
+++ proj/floyd/run_gpu.sh
@@ -1,1 +1,1 @@
-floyd run --gpu --env pytorch --mode jupyter --data dog-breed-identification:/data --data fatbunny/projects/dbi/3/output:/prev_output 
+floyd run --gpu --env pytorch --mode jupyter --data dog-breed-identification:/data --data fatbunny/projects/dbi/4/output:/prev_output 
