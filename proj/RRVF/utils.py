e757047dd7b9bf724606d81b447b951203db99a3
xiaochuan-du <xiaochuan.du@healscitech.com>
2018-02-16 17:32:50 +0800

    Update dependency

--- proj/RRVF/utils.py
+++ proj/RRVF/utils.py
@@ -435,3 +435,237 @@ def add_attr_static(tidy_df, data_statics, attrs):
         ]], how='left', on=key)
 
     return tidy_df, [], list(set(contins) - set(pre_vars))
+
+
+def fix_missing(df, col, name, na_dict):
+    """ Fill missing data in a column of df with the median, and add a {name}_na column
+    which specifies if the data was missing.
+
+    Parameters:
+    -----------
+    df: The data frame that will be changed.
+
+    col: The column of data to fix by filling in missing data.
+
+    name: The name of the new filled column in df.
+
+    na_dict: A dictionary of values to create na's of and the value to insert. If
+        name is not a key of na_dict the median will fill any missing data. Also
+        if name is not a key of na_dict and there is no missing data in col, then
+        no {name}_na column is not created.
+
+
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, np.NaN, 3], 'col2' : [5, 2, 2]})
+    >>> df
+       col1 col2
+    0     1    5
+    1   nan    2
+    2     3    2
+
+    >>> fix_missing(df, df['col1'], 'col1', {})
+    >>> df
+       col1 col2 col1_na
+    0     1    5   False
+    1     2    2    True
+    2     3    2   False
+
+
+    >>> df = pd.DataFrame({'col1' : [1, np.NaN, 3], 'col2' : [5, 2, 2]})
+    >>> df
+       col1 col2
+    0     1    5
+    1   nan    2
+    2     3    2
+
+    >>> fix_missing(df, df['col2'], 'col2', {})
+    >>> df
+       col1 col2
+    0     1    5
+    1   nan    2
+    2     3    2
+
+
+    >>> df = pd.DataFrame({'col1' : [1, np.NaN, 3], 'col2' : [5, 2, 2]})
+    >>> df
+       col1 col2
+    0     1    5
+    1   nan    2
+    2     3    2
+
+    >>> fix_missing(df, df['col1'], 'col1', {'col1' : 500})
+    >>> df
+       col1 col2 col1_na
+    0     1    5   False
+    1   500    2    True
+    2     3    2   False
+    """
+    if is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+def numericalize(df, col, name, max_n_cat):
+    """ Changes the column col from a categorical type to it's integer codes.
+
+    Parameters:
+    -----------
+    df: A pandas dataframe. df[name] will be filled with the integer codes from
+        col.
+
+    col: The column you wish to change into the categories.
+    name: The column name you wish to insert into df. This column will hold the
+        integer codes.
+
+    max_n_cat: If col has more categories than max_n_cat it will not change the
+        it to its integer codes. If max_n_cat is None, then col will always be
+        converted.
+
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    note the type of col2 is string
+
+    >>> train_cats(df)
+    >>> df
+
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    now the type of col2 is category { a : 1, b : 2}
+
+    >>> numericalize(df, df['col2'], 'col3', None)
+
+       col1 col2 col3
+    0     1    a    1
+    1     2    b    2
+    2     3    a    1
+    """
+    if not is_numeric_dtype(col) and ( max_n_cat is None or col.nunique()>max_n_cat):
+        df[name] = col.cat.codes+1
+
+def scale_vars(df, mapper):
+    warnings.filterwarnings('ignore', category=sklearn.exceptions.DataConversionWarning)
+    if mapper is None:
+        map_f = [([n],StandardScaler()) for n in df.columns if is_numeric_dtype(df[n])]
+        mapper = DataFrameMapper(map_f).fit(df)
+    df[mapper.transformed_names_] = mapper.transform(df)
+    return mapper
+
+def proc_df(df, y_fld, skip_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+
+    """ proc_df takes a data frame df and splits off the response variable, and
+    changes the df into an entirely numeric dataframe.
+
+    Parameters:
+    -----------
+    df: The data frame you wish to process.
+
+    y_fld: The name of the response variable
+
+    skip_flds: A list of fields that dropped from df.
+
+    do_scale: Standardizes each column in df,Takes Boolean Values(True,False)
+
+    na_dict: a dictionary of na columns to add. Na columns are also added if there
+        are any missing values.
+
+    preproc_fn: A function that gets applied to df.
+
+    max_n_cat: The maximum number of categories to break into dummy values, instead
+        of integer codes.
+
+    subset: Takes a random subset of size subset from df.
+
+    mapper: If do_scale is set as True, the mapper variable
+        calculates the values used for scaling of variables during training time(mean and standard deviation).
+
+    Returns:
+    --------
+    [x, y, nas, mapper(optional)]:
+
+        x: x is the transformed version of df. x will not have the response variable
+            and is entirely numeric.
+
+        y: y is the response variable
+
+        nas: returns a dictionary of which nas it created, and the associated median.
+
+        mapper: A DataFrameMapper which stores the mean and standard deviation of the corresponding continous
+        variables which is then used for scaling of during test-time.
+
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    note the type of col2 is string
+
+    >>> train_cats(df)
+    >>> df
+
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+
+    now the type of col2 is category { a : 1, b : 2}
+
+    >>> x, y, nas = proc_df(df, 'col1')
+    >>> x
+
+       col2
+    0     1
+    1     2
+    2     1
+
+    >>> data = DataFrame(pet=["cat", "dog", "dog", "fish", "cat", "dog", "cat", "fish"],
+                 children=[4., 6, 3, 3, 2, 3, 5, 4],
+                 salary=[90, 24, 44, 27, 32, 59, 36, 27])
+
+    >>> mapper = DataFrameMapper([(:pet, LabelBinarizer()),
+                          ([:children], StandardScaler())])
+
+    >>>round(fit_transform!(mapper, copy(data)), 2)
+
+    8x4 Array{Float64,2}:
+    1.0  0.0  0.0   0.21
+    0.0  1.0  0.0   1.88
+    0.0  1.0  0.0  -0.63
+    0.0  0.0  1.0  -0.63
+    1.0  0.0  0.0  -1.46
+    0.0  1.0  0.0  -0.63
+    1.0  0.0  0.0   1.04
+    0.0  0.0  1.0   0.21
+    """
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    df = df.copy()
+    if preproc_fn: preproc_fn(df)
+    y = df[y_fld].values
+    df.drop(skip_flds+[y_fld], axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    res = [pd.get_dummies(df, dummy_na=True), y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
